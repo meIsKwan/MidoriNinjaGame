cdb530b73d8370c9dcf46cc0bcdb777b37603dd1
meiskwan <mc.minhquan612@gmail.com>
2025-04-10 22:18:54 +0700

    Commit second ready for Section 2: 7 <Add Walk Animations>

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,15 +2,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Config")]
+    [SerializeField] private float speed;
+    private PlayerActions actions;
+    private Rigidbody2D rb2D;
+    private Vector2 moveDirection;
+
+    private void Awake()
     {
-        
+        actions = new PlayerActions();
+        rb2D = GetComponent<Rigidbody2D>();
     }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
 
     // Update is called once per frame
     void Update()
     {
-        
+        ReadMovement();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb2D.MovePosition(rb2D.position + moveDirection * (speed * Time.fixedDeltaTime));
+    }
+
+    private void ReadMovement()
+    {
+        moveDirection = actions.Movement.Move.ReadValue<Vector2>().normalized;
+    }
+
+    private void OnEnable()
+    {
+        actions.Enable();
+    }
+
+    private void OnDisable()
+    {
+        actions.Disable();
     }
 }
